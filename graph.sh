3a896f3683a79ae76dd76796e5449c5d77af9844
Teslim Olayiwola <olayiwolateslim9@gmail.com>
2024-09-12 14:14:17 -0500

    graph submission script

---
+++ graph.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Define variables
+MAIN_DIRECTORY="/Users/gbemidebe/Documents/GitHub/BatteryInformatics"
+RESULT_DIRECTORY="results"
+DATA_DIRECTORY="data"
+PYTHON_SCRIPT="$main_directory/src/graph/trainer.py"
+seed=42
+iterations=100
+cv=10
+train_data="train_data_cleaned.csv"
+test_data="test_data_cleaned.csv"
+
+
+# Run the Python script with arguments
+python "$PYTHON_SCRIPT" \
+    --parent_directory "$MAIN_DIRECTORY" \
+    --result_directory "$RESULT_DIRECTORY" \
+    --data_directory "$DATA_DIRECTORY" \
+    --train_data "$train_data" \
+    --test_data "$test_data" \
+    --seed $seed \
+    --epoch $iterations \
+    --cv $cv \
+    --train \
+    #--skip_cv \
